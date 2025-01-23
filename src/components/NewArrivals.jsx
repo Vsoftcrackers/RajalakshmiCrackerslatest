5c5c6c4c37389d356410a1f389b44a168188ea9d
Vsoftcrackers <vsoftsolutions8813@gmail.com>
2025-01-23 16:09:40 +0530

    Update NewArrivals.jsx

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -32,16 +32,16 @@ const NewArrivals = () => {
   };
 
   const images = [
-    { src: "assets/newarrivals1.webp", name: "Elegant Gold Necklace" },
-    { src: "assets/newarrivals2.webp", name: "Classic Gold Necklace" },
-    { src: "assets/newarrivals3.webp", name: "Emerald Bangle" },
-    { src: "assets/newarrivals4.webp", name: "Gold Earring" },
-    { src: "assets/newarrivals5.webp", name: "Ruby Stud Earrings" },
-    { src: "assets/newarrivals6.webp", name: "Gold Cuff Chain" },
-    { src: "assets/newarrivals7.webp", name: "Sapphire Kid Bracelet" },
-    { src: "assets/newarrivals8.webp", name: "Mangalasutra" },
-    { src: "assets/newarrivals9.webp", name: "Gold Emerald Necklace" },
-    { src: "assets/newarrivals10.webp", name: "Gold Stud Chain" },
+    { src: "assets/newarrivals1.webp", name: "crackers" },
+    { src: "assets/newarrivals2.webp", name: "crackers" },
+    { src: "assets/newarrivals3.webp", name: "crackers" },
+    { src: "assets/newarrivals4.webp", name: "crackers" },
+    { src: "assets/newarrivals5.webp", name: "crackers" },
+    { src: "assets/newarrivals6.webp", name: "crackers" },
+    { src: "assets/newarrivals7.webp", name: "crackers" },
+    { src: "assets/newarrivals8.webp", name: "crackers" },
+    { src: "assets/newarrivals9.webp", name: "crackers" },
+    { src: "assets/newarrivals10.webp", name: "crackers" },
   ];
 
   return (
@@ -67,3 +67,4 @@ const NewArrivals = () => {
 };
 
 export default NewArrivals;
+
