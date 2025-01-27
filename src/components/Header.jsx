d85adbbbbbc7105a74297ac4019524f145b18804
Sankar kumar <136483424+sankarsantosh99@users.noreply.github.com>
2025-01-27 16:42:16 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { IoLocationOutline } from "react-icons/io5"; // Location icon
-import { GiRotaryPhone } from "react-icons/gi";
-import CustomDropdown from "./CustomDropdown"; // Import the CustomDropdown component
+import React from "react";
 import { Link } from "react-router-dom"; // Import Link
 import "./Header.css"; // Header styles
 
@@ -9,28 +6,24 @@ const Header = () => {
 
   return (
     <header className="header">
-   <div className="logo-container">
-  <Link to="/">
-    <img
-      src="assets/spinner.png"
-      alt="Logo"
-      className="logo"
-      style={{ width: "130px", height: "55px" }}
-    />
-  </Link>
-</div>
+      <div className="logo-container">
+        <Link to="/">
+          <img
+            src="assets/spinner.png"
+            alt="Logo"
+            className="logo"
+            style={{ width: "130px", height: "60px" }}
+          />
+        </Link>
+      </div>
+      <div className="headlinks">
+        <Link to="/" className="links">Home</Link>
 
-      <div className="search-container">
-        {/*<CustomDropdown
-          options={[
-            { label: "Gold Rings", value: "gold-jewellery#rings" },
-            { label: "Gold Bangles", value: "gold-jewellery#bangles" },
-            { label: "Diamond Rings", value: "diamond-jewellery#rings" },
-            { label: "Silver Articles", value: "silver-collection#articles" },
-            { label: "Schemes (DigiGold)", value: "digigold" },
-          ]}
-          placeholder="Search here..."
-        />*/}
+        <Link to="/products" className="blink links">Pricelist</Link>
+
+        <Link to="/about" className="links">About Us</Link>
+
+        <Link to="/enquiry" className="links">Contact Us</Link>
       </div>
     </header>
   );
